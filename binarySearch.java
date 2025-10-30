e40bae6d4c1d2e9d7efbbe66e48766f169fa669b
sakshikgit <ssakshikourav@gmail.com>
2025-10-30 11:39:12 +0530

    sfnjdfn

--- binarySearch.java
+++ binarySearch.java
@@ -4,6 +4,9 @@ public class binarySearch {
         int arr[] = {2,5,7,11,45,87,90};
         binarysearch(arr, 87);
 
+        int arr2[] = {90, 88, 87, 56, 36, 22, 1};
+        binarySearchDesc(arr, 22);
+
     }
 
     static void binarysearch(int arr[], int target){
@@ -34,11 +37,39 @@ public class binarySearch {
         else{
             System.out.println("target found at index: " +ans );
         }
+    }
 
 
+    static void binarySearchDesc(int arr[], int target){
+        int ans = -1;
+        int start = 0;
+        int end = arr.length - 1;
 
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(arr[mid] == target){
+                ans = target;
+            }
+            else if(arr[mid] < target){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        if(ans == -1){
+            System.out.println("element not found");
+        }
+        else{
+            System.out.println("element found at index " + ans);
+        }
     }
 
 
     
+
+
+
+
 }
